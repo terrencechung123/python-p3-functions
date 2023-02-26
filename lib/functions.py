eaa05de429f12208b85e81fafcd9580dc2b9e9ca
Terrence Chung <terrencechung35@gmail.com>
2023-02-25 22:49:21 -0800

    lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print('Hello, programmer!')
     pass
 
 def greet(name):
-    pass
+    greet_person = print(f'Hello, {name}!')
+    return greet_person
 
 def greet_with_default(name="programmer"):
-    pass
+    greet_with_default=print(f'Hello, {name}!')
+    return greet_with_default
 
 def add(num1, num2):
-    pass
+    add = num1 + num2
+    return add
+
 
 def halve(number):
+    halve = number/2
+    return halve
     pass
